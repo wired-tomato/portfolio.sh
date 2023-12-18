22d429b64a12eebdd8dafb4a3a7742e9e85765fb
wired-tomato <brokenfuse.self@gmail.com>
2023-12-17 19:10:37 -0500

    Tomatoify

--- next.config.js
+++ next.config.js
@@ -1,1 +1,4 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {};
--- src/utils/tabCompletion.ts
+++ src/utils/tabCompletion.ts
@@ -4,9 +4,9 @@ export const handleTabCompletion = (
   command: string,
   setCommand: React.Dispatch<React.SetStateAction<string>>,
 ) => {
-  const commands = Object.keys(bin).filter((entry) =>
-    entry.startsWith(command),
-  );
+  const commands = Object.keys(bin)
+    .concat('clear', './portfolio.sh')
+    .filter((entry) => entry.startsWith(command));
 
   if (commands.length === 1) {
     setCommand(commands[0]);
